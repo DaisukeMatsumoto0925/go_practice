0f3860f0923cda244de349968400fe777546b36d
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-11-07 11:53:30 +0900

    create handler and view file

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"go-trading/app/controllers"
-	"go-trading/app/models"
 	"go-trading/config"
 	"go-trading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }

5b5a4bdf9838cb81dff57832d05db4a0f76ca438
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-11-10 20:41:35 +0900

    add functions

--- app/models/events.go
+++ app/models/events.go
@@ -1,14 +1,15 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
-	"go-trading/config"
 	"log"
 	"strings"
 	"time"
+
+	"go-trading/config"
 )
 
-// SignalEvent is model
 type SignalEvent struct {
 	Time        time.Time `json:"time"`
 	ProductCode string    `json:"product_code"`
@@ -17,7 +18,6 @@ type SignalEvent struct {
 	Size        float64   `json:"size"`
 }
 
-//Save is Insert sql
 func (s *SignalEvent) Save() bool {
 	cmd := fmt.Sprintf("INSERT INTO %s (time, product_code, side, price, size) VALUES (?, ?, ?, ?, ?)", tableNameSignalEvents)
 	_, err := DbConnection.Exec(cmd, s.Time.Format(time.RFC3339), s.ProductCode, s.Side, s.Price, s.Size)
@@ -31,17 +31,14 @@ func (s *SignalEvent) Save() bool {
 	return true
 }
 
-// SignalEvents is slice
 type SignalEvents struct {
 	Signals []SignalEvent `json:"signals,omitempty"`
 }
 
-// NewSignalEvents is
 func NewSignalEvents() *SignalEvents {
 	return &SignalEvents{}
 }
 
-// GetSignalEventsByCount is
 func GetSignalEventsByCount(loadEvents int) *SignalEvents {
 	cmd := fmt.Sprintf(`SELECT * FROM (
         SELECT time, product_code, side, price, size FROM %s WHERE product_code = ? ORDER BY time DESC LIMIT ? )
@@ -63,4 +60,137 @@ func GetSignalEventsByCount(loadEvents int) *SignalEvents {
 		return nil
 	}
 	return &signalEvents
+}
+
+func GetSignalEventsAfterTime(timeTime time.Time) *SignalEvents {
+	cmd := fmt.Sprintf(`SELECT * FROM (
+                SELECT time, product_code, side, price, size FROM %s
+                WHERE DATETIME(time) >= DATETIME(?)
+                ORDER BY time DESC
+            ) ORDER BY time ASC;`, tableNameSignalEvents)
+	rows, err := DbConnection.Query(cmd, timeTime.Format(time.RFC3339))
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+
+	var signalEvents SignalEvents
+	for rows.Next() {
+		var signalEvent SignalEvent
+		rows.Scan(&signalEvent.Time, &signalEvent.ProductCode, &signalEvent.Side, &signalEvent.Price, &signalEvent.Size)
+		signalEvents.Signals = append(signalEvents.Signals, signalEvent)
+	}
+	return &signalEvents
+}
+
+func (s *SignalEvents) CanBuy(time time.Time) bool {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
+		return true
+	}
+
+	lastSignal := s.Signals[lenSignals-1]
+	if lastSignal.Side == "SELL" && lastSignal.Time.Before(time) {
+		return true
+	}
+	return false
+}
+
+func (s *SignalEvents) CanSell(time time.Time) bool {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
+		return false
+	}
+
+	lastSignal := s.Signals[lenSignals-1]
+	if lastSignal.Side == "BUY" && lastSignal.Time.Before(time) {
+		return true
+	}
+	return false
+}
+
+func (s *SignalEvents) Buy(ProductCode string, time time.Time, price, size float64, save bool) bool {
+	if !s.CanBuy(time) {
+		return false
+	}
+	signalEvent := SignalEvent{
+		ProductCode: ProductCode,
+		Time:        time,
+		Side:        "BUY",
+		Price:       price,
+		Size:        size,
+	}
+	if save {
+		signalEvent.Save()
+	}
+	s.Signals = append(s.Signals, signalEvent)
+	return true
+}
+
+func (s *SignalEvents) Sell(productCode string, time time.Time, price, size float64, save bool) bool {
+
+	if !s.CanSell(time) {
+		return false
+	}
+
+	signalEvent := SignalEvent{
+		ProductCode: productCode,
+		Time:        time,
+		Side:        "SELL",
+		Price:       price,
+		Size:        size,
+	}
+	if save {
+		signalEvent.Save()
+	}
+	s.Signals = append(s.Signals, signalEvent)
+	return true
+}
+
+func (s *SignalEvents) Profit() float64 {
+	total := 0.0
+	beforeSell := 0.0
+	isHolding := false
+	for i, signalEvent := range s.Signals {
+		if i == 0 && signalEvent.Side == "SELL" {
+			continue
+		}
+		if signalEvent.Side == "BUY" {
+			total -= signalEvent.Price * signalEvent.Size
+			isHolding = true
+		}
+		if signalEvent.Side == "SEL" {
+			total += signalEvent.Price * signalEvent.Size
+			isHolding = false
+			beforeSell = total
+		}
+	}
+	if isHolding == true {
+		return beforeSell
+	}
+	return total
+}
+
+func (s SignalEvents) MarshalJSON() ([]byte, error) {
+	value, err := json.Marshal(&struct {
+		Signals []SignalEvent `json:"signals,omitempty"`
+		Profit  float64       `json:"profit,omitempty"`
+	}{
+		Signals: s.Signals,
+		Profit:  s.Profit(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return value, err
+}
+
+func (s *SignalEvents) CollectAfter(time time.Time) *SignalEvents {
+	for i, signal := range s.Signals {
+		if time.After(signal.Time) {
+			continue
+		}
+		return &SignalEvents{Signals: s.Signals[i:]}
+	}
+	return nil
 }

3daf163595c332d92761db16f1fddca2e93a9b0b
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-23 06:13:05 +0900

    updated

--- jwt-todo/main.go
+++ jwt-todo/main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/gin"
 )
-
-type User struct {
-	ID       uint64 `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Phone    string `json:"phone"`
-}
-
-//A sample use
-var user = User{
-	ID:       1,
-	Username: "username",
-	Password: "password",
-	Phone:    "49123454322", //this is a random number
-}
 
 var (
 	router = gin.Default()
@@ -34,13 +18,27 @@ func main() {
 	log.Fatal(router.Run(":8080"))
 }
 
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+}
+
+var user = User{
+	ID:       1,
+	Username: "username",
+	Password: "password",
+	Phone:    "49123454322", //this is a random number
+}
+
 func Login(c *gin.Context) {
 	var u User
 	if err := c.ShouldBindJSON(&u); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
 		return
 	}
-
+	//compare the user from the request, with the one we defined:
 	if user.Username != u.Username || user.Password != u.Password {
 		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
 		return
@@ -52,15 +50,15 @@ func Login(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, token)
 }
-
-func CreateToken(userid uint64) (string, error) {
+func CreateToken(userId uint64) (string, error) {
 	var err error
-	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd")
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
-	atClaims["user_id"] = userid
+	atClaims["user_id"] = userId
 	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-	at := jwt.NewWithClaims(jwt.SigningMethodES256, atClaims)
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	if err != nil {
 		return "", err

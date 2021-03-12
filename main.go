a5e0b77513dcd25021c087c780674681fde0e93e
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-12 11:15:20 +0900

    upd

--- main.go
+++ main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
+
+//増減管理
+func main() {
+	var n string
+	if sc.Scan() {
+		n = sc.Text()
+	}
+	nInt, _ := strconv.Atoi(n)
+	dailySales := make([]int, nInt)
+	for i := 0; i < cap(dailySales); i++ {
+		var sale string
+		if sc.Scan() {
+			sale = sc.Text()
+		}
+		saleInt, _ := strconv.Atoi(sale)
+
+		dailySales[i] = saleInt
+	}
+
+	for i := 0; i < cap(dailySales)-1; i++ {
+		var diff int
+		diff = dailySales[i+1] - dailySales[i]
+		if diff > 0 {
+			fmt.Printf("up %v\n", math.Abs(float64(diff)))
+		} else if diff < 0 {
+			fmt.Printf("down %v\n", math.Abs(float64(diff)))
+		} else {
+			fmt.Println("stay")
+		}
+	}
+}
 
 // 2倍チェック
 // func main() {

8f5727704b88f8d5b1093375b6e38bcf34d2bde6
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-13 07:56:47 +0900

    cmt

--- main.go
+++ main.go
@@ -1,62 +1,58 @@
 package main
 
-import (
-	"fmt"
-)
+// func main() {
+// 	var n, q int
+// 	fmt.Scan(&n, &q)
+// 	followList := make([][]int, n)
+// 	for i := range followList {
+// 		followList[i] = make([]int, n)
+// 	}
 
-func main() {
-	var n, q int
-	fmt.Scan(&n, &q)
-	followList := make([][]int, n)
-	for i := range followList {
-		followList[i] = make([]int, n)
-	}
-
-	for i := 0; i < q; i++ {
-		var m, a, b int
-		fmt.Scan(&m, &a)
-		if m == 1 {
-			fmt.Scan(&b)
-			followList[a-1][b-1] = 1
-		} else if m == 2 {
-			for j := 0; j < n; j++ {
-				if a-1 == j {
-					continue
-				}
-				if followList[j][a-1] > 0 {
-					followList[a-1][j] = 1
-				}
-			}
-		} else if m == 3 {
-			var addList []int
-			for j := 0; j < n; j++ {
-				if followList[a-1][j] > 0 {
-					addList = append(addList, j)
-				}
-			}
-			for _, j := range addList {
-				for k := 0; k < n; k++ {
-					if a-1 == k {
-						continue
-					}
-					if followList[j][k] > 0 {
-						followList[a-1][k] = 1
-					}
-				}
-			}
-		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if followList[i][j] > 0 {
-				fmt.Print("Y")
-			} else {
-				fmt.Printf("N")
-			}
-		}
-		fmt.Print("\n")
-	}
-}
+// 	for i := 0; i < q; i++ {
+// 		var m, a, b int
+// 		fmt.Scan(&m, &a)
+// 		if m == 1 {
+// 			fmt.Scan(&b)
+// 			followList[a-1][b-1] = 1
+// 		} else if m == 2 {
+// 			for j := 0; j < n; j++ {
+// 				if a-1 == j {
+// 					continue
+// 				}
+// 				if followList[j][a-1] > 0 {
+// 					followList[a-1][j] = 1
+// 				}
+// 			}
+// 		} else if m == 3 {
+// 			var addList []int
+// 			for j := 0; j < n; j++ {
+// 				if followList[a-1][j] > 0 {
+// 					addList = append(addList, j)
+// 				}
+// 			}
+// 			for _, j := range addList {
+// 				for k := 0; k < n; k++ {
+// 					if a-1 == k {
+// 						continue
+// 					}
+// 					if followList[j][k] > 0 {
+// 						followList[a-1][k] = 1
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	for i := 0; i < n; i++ {
+// 		for j := 0; j < n; j++ {
+// 			if followList[i][j] > 0 {
+// 				fmt.Print("Y")
+// 			} else {
+// 				fmt.Printf("N")
+// 			}
+// 		}
+// 		fmt.Print("\n")
+// 	}
+// }
 
 // 重複チェック
 // var sc = bufio.NewScanner(os.Stdin)

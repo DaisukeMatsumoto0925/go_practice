4d3b2ef7d42c5fb35c0489bb6b64fd43f23f91e1
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-04-19 08:23:33 +0900

    upd

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"time"
 )
 
 type Message string
@@ -11,19 +14,30 @@ func NewMessage() Message {
 }
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 type Greeter struct {
 	Message Message // <- adding a Message field
+	Grumpy  bool
 }
 
 func (g Greeter) Greet() Message {
+	if g.Grumpy {
+		return Message("Go away!")
+	}
 	return g.Message
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	if g.Grumpy {
+		return Event{}, errors.New("could not create event: event greeter is grumpy")
+	}
+	return Event{Greeter: g}, nil
 }
 
 type Event struct {
@@ -36,7 +50,10 @@ func (e Event) Start() {
 }
 
 func main() {
-	e := InitializeEvent()
-
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
 	e.Start()
 }

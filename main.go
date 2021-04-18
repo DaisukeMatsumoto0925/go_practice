b0be579c8a9e875ae36b6adc64c3b2b3f318715d
xfpng345 <daisuke.matsumoto0925@gmail.com>
2021-04-18 12:50:43 +0900

    upd

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
 
 type (
 	Message string
 	Greeter struct {
 		Message Message
+		Grumpy  bool
 	}
 
 	Event struct {
@@ -18,27 +24,36 @@ func NewMessage() Message {
 }
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 func (g Greeter) Greet() Message {
+	if g.Grumpy {
+		return Message("Go away!")
+	}
 	return g.Message
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	if g.Grumpy {
+		return Event{}, errors.New("could not create event: event greeter is grumpy")
+	}
+	return Event{Greeter: g}, nil
 }
 
 func (e Event) Start() {
 	msg := e.Greeter.Greet()
 	fmt.Println(msg)
 }
-
 func main() {
-	// message := NewMessage()
-	// greeter := NewGreter(message)
-	// event := NewEvent(greeter)
-	e := InitializeEvent()
-
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
 	e.Start()
 }

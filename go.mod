e3cf5bc3d5e90b3e9ff99da2c267ac26a4c8b8c6
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-05-30 14:37:03 +0900

    point1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DaisukeMatsumoto0925/go_embedding
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Flyer interface {
+	Fly() string
+}
+
+type Runner interface {
+	Run() string
+}
+
+type FlyingRunner interface {
+	Flyer
+	Runner
+}
+
+type ToriJin struct {
+	FlyingRunner
+}
+
+type ShinJinrui struct {
+	*ToriJin
+}
+
+type RealToriJin struct{}
+
+func (r RealToriJin) Fly() string {return "Fly!"}
+
+func (r RealToriJin) Run() string {return "Run!"}
+
+func main() {
+	aRealToriJin := &RealToriJin{}
+	aTorijin := &ToriJin{
+		FlyingRunner: aRealToriJin,
+	}
+	aShinJinrui := &ShinJinrui{
+		ToriJin: aTorijin,
+	}
+	fmt.Println(aShinJinrui.Fly())
+	fmt.Println(aShinJinrui.Run())
+}

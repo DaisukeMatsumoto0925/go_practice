86c4b8464fa1b901768f12c0f77cf1ce8d8acf0a
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-06-04 12:03:39 +0900

    add go_embedding

---
+++ go_embedding/status/status.go
@@ -0,0 +1,49 @@
+package status
+
+import "fmt"
+
+type Status int
+
+const (
+	Good Status = iota
+	Tired
+)
+
+func (s Status) String() string {
+	switch s {
+	case Good:
+		return "Good!"
+	case Tired:
+		return "Tired..."
+	default:
+		return ""
+	}
+}
+
+type poorGrasshopper struct {
+	status Status
+}
+
+func (g *poorGrasshopper) HighJUmp() {
+	fmt.Println("High Jump!")
+	g.status = Tired
+}
+
+type ShinJinrui3 struct{
+	status Status
+	*poorGrasshopper
+}
+
+func InitStatus() {
+	aPoorGrasshoper := &poorGrasshopper{
+		status: Good,
+	}
+	aShinJinrui3 := &ShinJinrui3{
+		status:  Good,
+		poorGrasshopper: aPoorGrasshoper,
+	}
+	aShinJinrui3.HighJUmp()
+	fmt.Println("aPoorGrasshopper is", aPoorGrasshoper.status)
+	fmt.Println("aShinJinrui3 is", aShinJinrui3.status)
+	fmt.Println("aShinJinrui3 is", aShinJinrui3.poorGrasshopper.status)
+}

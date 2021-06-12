9f469ae35603a785bdcf35a9816dd419ebc93239
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-06-12 13:42:09 +0900

    updated

--- go_pointa/main.go
+++ go_pointa/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+// Person は人間を表す構造体。
+type Person struct {
+    Name string
+    Age  int
+}
+
 func main() {
-	fmt.Println("hellow world")
+    // ポインタ型の変数を宣言する
+    // pがポインタ型変数
+    // *Personポインタ型
+    var p *Person
+
+    p = &Person{
+        Name: "太郎",
+        Age:  20,
+    }
+    fmt.Printf("p :%+v\n", p)
+    fmt.Printf("変数pに格納されているアドレス :%p", p)
 }

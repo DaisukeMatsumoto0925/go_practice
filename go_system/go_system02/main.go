ef3ffbef871552f783397dc61ea64d8f3ef86425
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-06-12 14:40:52 +0900

    updated

---
+++ go_system/go_system02/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"net"
+	"os"
+)
+
+// func main() {
+//     file, err := os.Create("test.txt")
+//     if err != nil {
+//         panic(err)
+//     }
+//     file.Write([]byte("os.File example\n"))
+//     file.Close()
+// }
+
+// func main() {
+//     os.Stdout.Write([]byte("os.Stdout example\n"))
+// }
+
+// func main() {
+//     var buffer bytes.Buffer
+//     buffer.Write([]byte("bytes.Buffer example\n"))
+//     fmt.Println(buffer.String())
+// }
+
+
+func main() {
+    conn, err := net.Dial("tcp", "ascii.jp:80")
+    if err != nil {
+    panic(err)
+    }
+    conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+    io.Copy(os.Stdout, conn)
+}

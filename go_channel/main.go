22b0b893c75da641e64f766664741e9a523d5dcc
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-06-12 19:46:13 +0900

    updated

--- go_channel/main.go
+++ go_channel/main.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
 	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "two"
-	}()
-
-	go func() {
-		time.Sleep(1 * time.Second)
-		c1 <- "one"
-	}()
-
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
 		}
+	}()
+
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
 	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
 }

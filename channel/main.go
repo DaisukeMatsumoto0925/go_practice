93fcc6e486d8cef7f054d3b9b26c04a76174930e
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-08-22 11:29:17 +0900

    upd

--- channel/main.go
+++ channel/main.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type User struct {
-	ID        int       `json:"id"`
-	Email     string    `json:"email"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
+// type User struct {
+// 	ID        int       `json:"id"`
+// 	Email     string    `json:"email"`
+// 	Name      string    `json:"name"`
+// 	CreatedAt time.Time `json:"created_at"`
+// 	UpdatedAt time.Time `json:"updated_at"`
+// }
 
-type Sub struct {
-	userChannels map[int]chan *User
-}
+// type Sub struct {
+// 	userChannels map[int]chan *User
+// }
+
+// func main() {
+// 	// userChannel := make(map[string]chan *User, 2)
+// 	userChan := make(chan *User, 1)
+
+// 	sub := &Sub{
+// 		userChannels: map[int]chan *User{},
+// 	}
+
+// 	for i := 0; i < 10; i++ {
+// 		sub.userChannels[i] = userChan
+// 	}
+
+// 	fmt.Println(&sub.userChannels)
+// }
 
 func main() {
-	// userChannel := make(map[string]chan *User, 2)
-	userChan := make(chan *User, 1)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-	sub := &Sub{
-		userChannels: map[int]chan *User{},
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
 	}
+	close(jobs)
+	fmt.Println("sent all jobs")
 
-	for i := 0; i < 10; i++ {
-		sub.userChannels[i] = userChan
-	}
-
-	fmt.Println(&sub.userChannels)
+	<-done
 }

ceffd414f838a4c8357810f94999c417ec8c9ed2
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-08-24 12:08:59 +0900

    updated

---
+++ aws_sdk/go.mod
@@ -0,0 +1,9 @@
+module github.com/DaisukeMatsumoto0925/aws_sdk
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.8.1
+	github.com/aws/aws-sdk-go-v2/config v1.6.1
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.13.0
+)
--- channel/main.go
+++ channel/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // type User struct {
@@ -58,25 +59,72 @@ import (
 // 	<-done
 // }
 
-// チャネルに値を渡す
-func sendValue(c chan<- int) {
-	for i := 0; i < 5; i++ {
-		c <- i
-	}
-	close(c)
-}
+// // チャネルに値を渡す
+// func sendValue(c chan<- int) {
+// 	for i := 0; i < 5; i++ {
+// 		c <- i
+// 	}
+// 	close(c)
+// }
 
-// チャネルから値を受け取る
-func receiveValue(c <-chan int) {
-	for v := range c {
-		fmt.Println("チャネルから受け取った値：", v)
-	}
-}
+// // チャネルから値を受け取る
+// func receiveValue(c <-chan int) {
+// 	for v := range c {
+// 		fmt.Println("チャネルから受け取った値：", v)
+// 	}
+// }
 
-func main() {
-	c := make(chan int)
-	go sendValue(c)
-	receiveValue(c)
+// func main() {
+// 	c := make(chan int)
+// 	go sendValue(c)
+// 	receiveValue(c)
 
-	fmt.Println("処理を終了します。")
-}
+// 	fmt.Println("処理を終了します。")
+// }
+
+// _Closing_ a channel indicates that no more values
+// will be sent on it. This can be useful to communicate
+// completion to the channel's receivers.
+// In this example we'll use a `jobs` channel to
+// communicate work to be done from the `main()` goroutine
+// to a worker goroutine. When we have no more jobs for
+// the worker we'll `close` the `jobs` channel.
+// func main() {
+// 	jobs := make(chan int, 5)
+// 	done := make(chan bool)
+
+// 	// Here's the worker goroutine. It repeatedly receives
+// 	// from `jobs` with `j, more := <-jobs`. In this
+// 	// special 2-value form of receive, the `more` value
+// 	// will be `false` if `jobs` has been `close`d and all
+// 	// values in the channel have already been received.
+// 	// We use this to notify on `done` when we've worked
+// 	// all our jobs.
+// 	go func() {
+// 		for {
+// 			j, more := <-jobs
+// 			if more {
+// 				time.Sleep(500)
+// 				fmt.Println("received job", j)
+// 			} else {
+// 				fmt.Println("received all jobs")
+// 				done <- true
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	// This sends 3 jobs to the worker over the `jobs`
+// 	// channel, then closes it.
+// 	for j := 1; j <= 3; j++ {
+// 		jobs <- j
+// 		fmt.Println("sent job", j)
+// 	}
+// 	close(jobs)
+// 	fmt.Println("sent all jobs")
+
+// 	// We await the worker using the
+// 	// [synchronization](channel-synchronization) approach
+// 	// we saw earlier.
+// 	<-done
+// }

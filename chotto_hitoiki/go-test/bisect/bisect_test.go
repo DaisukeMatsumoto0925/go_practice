1412b357b120ea96de463b1b1bc2dacf6d115f49
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-09-27 06:46:28 +0900

    add some testss

--- chotto_hitoiki/go-test/bisect/bisect_test.go
+++ chotto_hitoiki/go-test/bisect/bisect_test.go
@@ -15,3 +15,33 @@ func TestStop_block(t *testing.T) {
 		t.Errorf("get wrong ans: %d", ans)
 	}
 }
+
+func TestOver_left(t *testing.T) {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	key := -1
+
+	ans := bisect.BisectLeft(a, key)
+	if ans != 0 {
+		t.Errorf("get wrong ans: %d", ans)
+	}
+}
+
+func TestOver_right(t *testing.T) {
+	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
+	key := 9
+
+	ans := bisect.BisectLeft(a, key)
+	if ans != 9 {
+		t.Errorf("get wrong ans: %d", ans)
+	}
+}
+
+func TestNot__exist(t *testing.T) {
+	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
+	key := 6
+
+	ans := bisect.BisectLeft(a, key)
+	if ans != 7 {
+		t.Errorf("get wrong ans: %d", ans)
+	}
+}

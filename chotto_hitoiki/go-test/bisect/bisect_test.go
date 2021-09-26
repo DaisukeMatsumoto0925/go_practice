100cb123d54b4ce6934f94187c1ac138ec224806
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-09-27 07:38:26 +0900

    update to table driven

--- chotto_hitoiki/go-test/bisect/bisect_test.go
+++ chotto_hitoiki/go-test/bisect/bisect_test.go
@@ -6,42 +6,62 @@ import (
 	"github.com/DaisukeMatsumoto0925/go-test/bisect"
 )
 
-func TestStop_block(t *testing.T) {
-	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	key := 4
+// func TestStop_block(t *testing.T) {
+// 	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+// 	key := 4
 
-	ans := bisect.BisectLeft(a, key)
-	if ans != 3 {
-		t.Errorf("get wrong ans: %d", ans)
-	}
+// 	ans := bisect.BisectLeft(a, key)
+// 	if ans != 3 {
+// 		t.Errorf("get wrong ans: %d", ans)
+// 	}
+// }
+
+// func TestOver_left(t *testing.T) {
+// 	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+// 	key := -1
+
+// 	ans := bisect.BisectLeft(a, key)
+// 	if ans != 0 {
+// 		t.Errorf("get wrong ans: %d", ans)
+// 	}
+// }
+
+// func TestOver_right(t *testing.T) {
+// 	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
+// 	key := 9
+
+// 	ans := bisect.BisectLeft(a, key)
+// 	if ans != 9 {
+// 		t.Errorf("get wrong ans: %d", ans)
+// 	}
+// }
+
+// func TestNot__exist(t *testing.T) {
+// 	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
+// 	key := 6
+
+// 	ans := bisect.BisectLeft(a, key)
+// 	if ans != 7 {
+// 		t.Errorf("get wrong ans: %d", ans)
+// 	}
+// }
+
+var tests = []struct {
+	title    string
+	key, ans int
+}{
+	{title: "overLeft", key: -1, ans: 0},
+	{title: "stopEven", key: 2, ans: 1},
+	{title: "overLeft", key: 3, ans: 2},
 }
 
-func TestOver_left(t *testing.T) {
-	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	key := -1
-
-	ans := bisect.BisectLeft(a, key)
-	if ans != 0 {
-		t.Errorf("get wrong ans: %d", ans)
-	}
-}
-
-func TestOver_right(t *testing.T) {
+func TestTableDriven(t *testing.T) {
 	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
-	key := 9
-
-	ans := bisect.BisectLeft(a, key)
-	if ans != 9 {
-		t.Errorf("get wrong ans: %d", ans)
-	}
-}
-
-func TestNot__exist(t *testing.T) {
-	a := []int{1, 2, 3, 4, 4, 4, 5, 7, 8}
-	key := 6
-
-	ans := bisect.BisectLeft(a, key)
-	if ans != 7 {
-		t.Errorf("get wrong ans: %d", ans)
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			if got := bisect.BisectLeft(a, tt.key); got != tt.ans {
+				t.Errorf("%s get wrong ans %d: want %d", tt.title, got, tt.ans)
+			}
+		})
 	}
 }

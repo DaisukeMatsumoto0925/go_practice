ac00378a1b0859272bc22b5fc8652f65fc1f2408
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-09-28 09:42:11 +0900

    upd

--- chotto_hitoiki/go-test/main.go
+++ chotto_hitoiki/go-test/main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/DaisukeMatsumoto0925/go-test/bisect"
-)
+func add(m int, n int) int {
+	return m + n
+}
 
 func main() {
-	array := []int{1, 2, 3}
-	fmt.Println(bisect.BisectLeft(array, 1))
+	i := add(1, 2)
+	println(i)
 }

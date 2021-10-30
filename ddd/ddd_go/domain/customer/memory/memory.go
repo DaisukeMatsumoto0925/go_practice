c219d1bd98b2ed40017042c4fbb42ce140a9a44b
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-10-30 12:59:05 +0900

    imple memory repo

---
+++ ddd/ddd_go/domain/customer/memory/memory.go
@@ -0,0 +1,31 @@
+package memory
+
+import (
+	"sync"
+
+	"github.com/DaisukeMatsumoto0925/ddd_go/aggregate"
+	"github.com/google/uuid"
+)
+
+type MemoryRepository struct {
+	customers map[uuid.UUID]aggregate.Customer
+	sync.Mutex
+}
+
+func New() *MemoryRepository {
+	return &MemoryRepository{
+		customers: make(map[uuid.UUID]aggregate.Customer),
+	}
+}
+
+func (mr *MemoryRepository) Get(uuid.UUID) (aggregate.Customer, error) {
+	return aggregate.Customer{}, nil
+}
+
+func (mr *MemoryRepository) Add(aggregate.Customer) error {
+	return nil
+}
+
+func (mr *MemoryRepository) Update(aggregate.Customer) error {
+	return nil
+}

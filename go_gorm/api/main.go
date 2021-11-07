cd9acc19627c2fd504ca769167d437629147c731
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-11-07 22:20:04 +0900

    upd

--- go_gorm/api/main.go
+++ go_gorm/api/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type key string
+
+var (
+	transactionKey key = "transaction"
 )
 
 func main() {
 	fmt.Println("hello")
+}
+
+func transaction(
+	db *gorm.DB, ctx context.Context,
+	f func(context.Context) (interface{}, error),
+) (interface{}, error) {
+	tx := db.Begin()
+
+	ctx = setTx(ctx, tx)
+
+	v, err := f(ctx)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func setTx(ctx context.Context, tx *gorm.DB) context.Context {
+	return context.WithValue(ctx, transactionKey, tx)
 }
--- go_gorm/api/main_test.go
+++ go_gorm/api/main_test.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/DaisukeMatsumoto0925/api/src/infra/rdb"
+	"gorm.io/gorm"
 )
 
 func Test_main(t *testing.T) {
 	rdb := rdb.NewRDB()
+	rdb2 := newRdb()
 	fmt.Println(rdb.Config)
 
 	tx1 := rdb.Begin()
 	t.Cleanup(func() {
 		fmt.Println("Rollback!!")
-		tx1.Rollback()
+		if err := tx1.Rollback().Error; err != nil {
+			panic(err)
+		}
 	})
 
-	tx2 := rdb.Begin()
-	if err := tx2.Commit().Error; err != nil {
-		tx2.Rollback()
+	ctx := context.Background()
+	_, err := transaction(rdb2, ctx, func(ctx context.Context) (interface{}, error) {
+
+		return nil, nil
+	})
+	if err != nil {
 		fmt.Println(err)
 	}
 
+	// tx2 := rdb.Begin()
+	// if err := tx2.Commit().Error; err != nil {
+	// 	tx2.Rollback()
+	// 	fmt.Println(err)
+	// }
+
 	fmt.Println("finish")
 
+}
+
+func newRdb() *gorm.DB {
+	return rdb.NewRDB()
 }

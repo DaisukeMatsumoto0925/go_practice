763a1c4158bd7a00da875a208bb8fd1041e07c02
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-11-21 17:59:08 +0900

    gorilla

--- go_cookie/go.mod
+++ go_cookie/go.mod
@@ -1,3 +1,5 @@
 module github.com/DaisukeMatsumoto0925/go_cookie
 
 go 1.16
+
+require github.com/gorilla/sessions v1.2.1
--- go_cookie/main.go
+++ go_cookie/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/gorilla/sessions"
 )
 
 func setCookies(w http.ResponseWriter, r *http.Request) {
@@ -24,9 +26,31 @@ func showCookie(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, cookie)
 }
 
+var store = sessions.NewCookieStore([]byte("SESSION_KEY"))
+
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session-name")
+	session.Values["foo"] = "bar"
+	session.Values[42] = 43
+
+	session.Options.HttpOnly = true
+	session.Options.MaxAge = 10000
+	session.Options.Secure = true
+
+	session.AddFlash("foo")
+	session.AddFlash("bar")
+
+	err := session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", setCookies)
-	http.HandleFunc("/cookie", showCookie)
+	// http.HandleFunc("/", setCookies)
+	// http.HandleFunc("/cookie", showCookie)
+	http.HandleFunc("/gorilla", MyHandler)
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)

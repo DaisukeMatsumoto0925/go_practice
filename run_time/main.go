112f2a78193495fc975a97f610cc9e5c10266861
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2021-12-19 17:15:36 +0900

    update

--- run_time/main.go
+++ run_time/main.go
@@ -7,10 +7,35 @@ import (
 )
 
 func main() {
-	_, file, line, ok := runtime.Caller(0)
+	err := func01()
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = check("c", "d")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
+func check(a, b string) error {
+	if a == b {
+		return nil
+	}
+
+	msg := fmt.Sprintf(`check error. "%s" != "%s"`, a, b)
+	return makeError(msg, 2)
+}
+
+func func01() error {
+	return check("a", "b")
+}
+
+func makeError(msg string, skip int) error {
+	pc, file, line, ok := runtime.Caller(skip)
 	if ok {
 		name := filepath.Base(file)
-		fmt.Printf("file: %s, line: %d\n", name, line)
+		return fmt.Errorf(`%s; file: %s, line: %d, func: %s`,
+			msg, name, line, runtime.FuncForPC(pc).Name())
 	}
+	return fmt.Errorf("%s", msg)
 }

640489234dad2d0eefcbe4580837c8f7ce89e64d
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2022-01-02 18:06:56 +0900

    update

--- oapi_gen/main.go
+++ oapi_gen/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"log"
 
-	"github.com/DaisukeMatsumoto0925/oapi_gen/openapi"
+	"github.com/DaisukeMatsumoto0925/oapi_gen/PetstoreExpanded"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	r := echo.New()
-	si := &openapi.Handler{}
-	openapi.RegisterHandlersWithBaseURL(r, si, "api")
+	PetstoreExpanded.RegisterHandlersWithBaseURL(r, si, "api")
 	log.Fatal(r.Start(":8080"))
 
 }

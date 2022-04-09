f71e00b6b05f3e494e4722a951235d5a0500b3a1
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2022-04-09 23:39:30 +0900

    updated

--- atcoder/2placing_marbles/main.go
+++ atcoder/2placing_marbles/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	var in string
+	fmt.Scan(&in)
+
+	numbers := strings.Split(in, "")
+	squareHolder, err := newSquareHolder(numbers)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(squareHolder.count())
+}
+
+type Counter interface {
+	count() int
+}
+
+var _ Counter = (*squareHolder)(nil)
+
+type squareHolder struct {
+	first  int
+	second int
+	third  int
+}
+
+func newSquareHolder(numbers []string) (*squareHolder, error) {
+	if len(numbers) > 3 {
+		return nil, errors.New("invalid digit. must be 3digit")
+	}
+	first, err := strconv.Atoi(numbers[0])
+	if err != nil {
+		return nil, err
+	}
+	second, err := strconv.Atoi(numbers[1])
+	if err != nil {
+		return nil, err
+	}
+	third, err := strconv.Atoi(numbers[2])
+	if err != nil {
+		return nil, err
+	}
+	return &squareHolder{
+		first:  first,
+		second: second,
+		third:  third,
+	}, nil
+}
+
+func (s *squareHolder) count() int {
+	var counts int
+	if s.first == 1 {
+		counts++
+	}
+	if s.second == 1 {
+		counts++
+	}
+	if s.third == 1 {
+		counts++
+	}
+	return counts
 }
